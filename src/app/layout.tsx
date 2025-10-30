ff7092f2c7cf1e15eb28525bebb65a987472f53e
Ivan07071999 <ivan99mikh@gmail.com>
2025-10-30 18:19:24 +0300

    feat: week-6 complited

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { ThemeProvider } from '../context';
 import './globals.css';
+import Script from 'next/script';
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const metadata: Metadata = {
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" data-scroll-behavior="smooth">
       <body>
         <ThemeProvider>{children}</ThemeProvider>
+        <Script src="https://cdn.example.com/script.js" strategy="afterInteractive" />
       </body>
     </html>
   );
